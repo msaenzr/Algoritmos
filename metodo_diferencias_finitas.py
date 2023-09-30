34e24bded7bc618cb51b4d66ae1dc18ede755ca2
María José Sáenz <m.saenzr@uniandes.edu.co>
2023-09-30 15:11:58 -0500

    Final DF

--- metodo_diferencias_finitas.py
+++ metodo_diferencias_finitas.py
@@ -2,24 +2,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Dimensiones de la grafica
-l_x = 10
-l_y = 10
-
 #Condiciones de frontera
 b = 5
 y_0 = 0
 x_0 = x_1 = 0
 
-
-#Creación de grilla
+#Creación de grilla con dimensiones de la gráfica
+l_x = 10
+l_y = 10
 g = 0.1*100
 x, y = np.meshgrid(np.linspace(0,g,l_x),np.linspace(0,g,l_y))
 
-
-#Se fija el valor de interes en todas las celdas
-V = np.empty((l_x,l_y))
-V.fill(0)
+#Creación de matriz para almacenar valores del potencial
+V = np.zeros((l_x,l_y))
 
 #Evaluacion de condiciones de frontera
 V[(l_y-1):, :] = b
